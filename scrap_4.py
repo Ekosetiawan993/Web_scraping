edb1665361433df2ca7b156739a8926c654fbafb
Eko Setiawan <ekomasterwan993@gmail.com>
2021-07-06 14:03:17 +0700

    Finish the tutorial and will try to do a practice soon

--- scrap_4.py
+++ scrap_4.py
@@ -12,14 +12,21 @@ soup1 = BeautifulSoup(page1.content, "html.parser")
 result = soup.find(id="ResultsContainer")
 job_elements = result.find_all("div", class_="card-content")
 
+python_jobs = result.find_all(
+    "h2", string=lambda text: "python" in text.lower()
+)
 #print(result.prettify())
 #print(page.text)
 
-for job_element in job_elements:
+python_job_elements = [
+    h2_element.parent.parent.parent for h2_element in python_jobs
+]
+
+for job_element in python_job_elements:
     title_element = job_element.find("h2", class_="title")
     company_element = job_element.find("h3", class_="company")
     location_element = job_element.find("p", class_="location")
-    print(title_element)
-    print(company_element)
-    print(location_element)
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
     print()
